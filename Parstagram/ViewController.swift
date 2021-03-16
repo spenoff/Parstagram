2f0af9bbafaf8489b7b6cfe72ccd40caa2b7ffa2
snisonoff <spencer.nisonoff@stonybrook.edu>
2021-03-16 17:51:47 -0400

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Spencer Nisonoff on 3/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
