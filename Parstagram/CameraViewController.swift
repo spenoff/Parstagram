dbf28ee24d673350f484e4aaa8dd0690af2b11de
snisonoff <spencer.nisonoff@stonybrook.edu>
2021-03-19 23:46:41 -0400

    Added home screen icon

---
+++ Parstagram/CameraViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CameraViewController.swift
+//  Parstagram
+//
+//  Created by Spencer Nisonoff on 3/19/21.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var commentField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSubmitButton(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
